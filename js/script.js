b12e1a712b140425285ac970b38e6a9547637eab
Bakerman <74716816+DanilMetelkin@users.noreply.github.com>
2022-01-23 14:49:24 +0600

    Update files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+ 
+//  Burger-menu, scroll lock(body), 
  const burger = document.querySelector('.header__burger');
 const body = document.querySelector('body');
 const menu = document.querySelector('.header__list');
@@ -8,6 +10,7 @@ burger.classList.toggle('active');
 body.classList.toggle('lock');
 })
 
+// Tabs on table(index.html)
 const firstTab  = document.querySelector('.table__first-tab');
 const secondTab  = document.querySelector('.table__second-tab');
 
@@ -28,3 +31,19 @@ secondTab.addEventListener('click', () => {
         secondTab.classList.toggle('active');
     }
 })
+
+// min date for input in table (index.html)
+
+const dateOfDepart = document.querySelector('.table__date-of-depart');
+const dateOfArrive = document.querySelector('.table__date-of-arrive');
+const date = new Date();
+let currYear = date.getFullYear();
+let currMonth = date.getMonth() + 1;
+let currFullMonth = "0";
+if (currMonth.toString().length < 2) {
+    currFullMonth += currMonth.toString();
+}
+let currDate = date.getDate();
+let minDate = currYear + '-' + currFullMonth + '-' + currDate;
+dateOfDepart.setAttribute('min', minDate);
+dateOfArrive.setAttribute('min', minDate);
